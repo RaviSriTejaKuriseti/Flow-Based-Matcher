7437a45b8d8f5b772c0025ad206aebdf1145fc9a
ajayshastry08 <95415190+ajayshastry08@users.noreply.github.com>
2023-10-28 15:10:23 +0530

    Add files via upload

---
+++ install_environment.sh
@@ -0,0 +1,19 @@
+
+ conda create -n flow_matcher python=3.8
+ conda activate flow_matcher
+ pip install numpy
+ pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+ pip install pandas
+ pip install termcolor
+ pip install addict
+ pip install yapf
+ pip install git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI&egg=pycocotools
+ pip install opencv-python
+ pip install timm
+ pip install scipy
+ pip install ortools
+ pip install typing_extensions
+ pip install requests
+ pip install git+https://github.com/cocodataset/panopticapi.git#egg=panopticapi
+
+
